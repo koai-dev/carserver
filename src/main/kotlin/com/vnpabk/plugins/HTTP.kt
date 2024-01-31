8237ffa920711347ea0e958aece1dc0b0cfc9566
cpe02.khanhkn <cpe02.khanhkn@techcombank.com.vn>
2024-01-31 14:44:40 +0700

    update gradle

--- src/main/kotlin/com/vnpabk/plugins/HTTP.kt
+++ src/main/kotlin/com/vnpabk/plugins/HTTP.kt
@@ -1,10 +1,14 @@
 package com.vnpabk.plugins
 
+import io.ktor.http.*
 import io.ktor.server.application.*
 import io.ktor.server.plugins.defaultheaders.*
 
 fun Application.configureHTTP() {
     install(DefaultHeaders) {
-        header("X-Engine", "Ktor") // will send this header with each response
+        header("X-Engine", "Ktor")
+        header(HttpHeaders.ContentType, "application/json; charset=UTF-8; multipart/form-data")
+        header(HttpHeaders.Accept, "*/*")
+        header(HttpHeaders.ContentDisposition,"form-data")
     }
 }
